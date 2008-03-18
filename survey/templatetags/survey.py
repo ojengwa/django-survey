c77914f25d8c1e672e0ff444ebd4f03f9fc9c151
lionel.hummel <lionel.hummel@f67e0a98-9c48-0410-a59e-4384d0670f6d>
2008-03-18 23:20:42 +0000

    Removed unused imports

--- survey/templatetags/survey.py
+++ survey/templatetags/survey.py
@@ -1,14 +1,6 @@
 from django import template
-from django.conf import settings
-from django.core.cache import cache
-from cPickle import loads, dumps, HIGHEST_PROTOCOL
-import datetime
-#import feedparser
-import re
-import time
 
 register = template.Library()
-
 
 @register.filter
 def has_answered(request, survey):
@@ -18,7 +10,3 @@ def has_answered(request, survey):
 @register.filter
 def can_view_answers(user, survey):
     return survey.answers_viewable_by(user)
-
-
-
-
