b3bde7c72bc0a5160455dac04ac106c7636ac684
yann.malet <yann.malet@f67e0a98-9c48-0410-a59e-4384d0670f6d>
2008-10-30 00:58:39 +0000

    Fix #32 patch contibuted by scanner

--- survey/urls.py
+++ survey/urls.py
@@ -33,12 +33,12 @@ urlpatterns = patterns('',
     url(r'^delete/(?P<survey_slug>[-\w]+)/$', survey_delete, name='survey-delete'),
 
     url(r'^question/add/(?P<survey_slug>[-\w]+)/$', question_add,   name='question-add'),
-    url(r'^question/update/(?P<survey_slug>[-\w]+)/(?P<question_id>\d)/$', question_update,   name='question-update'),
-    url(r'^question/delete/(?P<survey_slug>[-\w]+)/(?P<question_id>\d)/$', question_delete,   name='question-delete'),
+    url(r'^question/update/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', question_update,   name='question-update'),
+    url(r'^question/delete/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', question_delete,   name='question-delete'),
 
-    url(r'^choice/add/(?P<question_id>\d)/$', choice_add,   name='choice-add'),
-    url(r'^choice/update/(?P<question_id>\d)/(?P<choice_id>\d)/$', choice_update,   name='choice-update'),
-    url(r'^choice/delete/(?P<survey_slug>[-\w]+)/(?P<choice_id>\d)/$', choice_delete,   name='choice-delete'),
+    url(r'^choice/add/(?P<question_id>\d+)/$', choice_add,   name='choice-add'),
+    url(r'^choice/update/(?P<question_id>\d+)/(?P<choice_id>\d+)/$', choice_update,   name='choice-update'),
+    url(r'^choice/delete/(?P<survey_slug>[-\w]+)/(?P<choice_id>\d+)/$', choice_delete,   name='choice-delete'),
 
-    url(r'^delete_image/(?P<model_string>[-\w]+)/(?P<object_id>\d)/$', delete_image, name='delete-image'),
+    url(r'^delete_image/(?P<model_string>[-\w]+)/(?P<object_id>\d+)/$', delete_image, name='delete-image'),
     )
